3ad7b3ea27b4896411a2905d3ad09c52f470201c
ErminBejtula <44189228+ErminBejtula@users.noreply.github.com>
2019-03-20 13:04:38 +0100

    Dags och designa antar jag

---
+++ src/view/Main.java
@@ -0,0 +1,29 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(this.getClass().getResource("ProjektLedning.fxml"));
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Projektledning");
+			primaryStage.setMinHeight(500);
+			primaryStage.setMinWidth(700);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
