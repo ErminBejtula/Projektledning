231d23428a642f68e9bfa1776c7a5a4dee46f20a
ErminBejtula <44189228+ErminBejtula@users.noreply.github.com>
2019-03-20 12:40:19 +0100

    Model finished

---
+++ src/model/Employee.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Employee {
+	
+	String phoneNumber;
+	String name;
+	int age;
+	String mail;
+	String language;
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+}
